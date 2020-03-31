4fe990a00e2d4b62f66e79c5fdbb4746f37994c3
Phil de Joux <philderbeast@gmail.com>
2020-03-31 08:59:07 -0400

    Pull some site.hs changes across from blockscope's.

--- www-hakyll/site.hs
+++ www-hakyll/site.hs
@@ -1,7 +1,59 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Monoid (mappend)
+import Control.Applicative (empty)
+import Data.List (intersperse)
+import Data.Monoid ((<>))
 import Hakyll
+    ( Context(..), Item(..), Configuration(..)
+    , Rules, Pattern, Compiler, Identifier
+    , defaultContext
+    , dateField
+    , tagsFieldWith
+    , constField
+    , listField
+    , loadAll
+    , loadAndApplyTemplate
+    , templateCompiler
+    , copyFileCompiler
+    , match
+    , compile
+    , relativizeUrls
+    , route
+    , idRoute
+    , recentFirst
+    , create
+    , makeItem
+    , setExtension
+    , fromGlob
+    , fromCapture
+    , pandocCompilerWith
+    , hakyllWith
+    , defaultHakyllReaderOptions
+    , defaultHakyllWriterOptions
+    , defaultConfiguration
+    , toUrl
+    , fromList
+    , applyAsTemplate
+    , getResourceBody
+    )
+import Hakyll.Web.Tags (Tags, buildTags, tagsRules, getTags)
+import Text.Pandoc.Options
+    (ReaderOptions(..), WriterOptions(..), HTMLMathMethod(..))
+import Text.Blaze.Html ((!), toHtml, toValue)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+pandocReaderOptions :: ReaderOptions
+pandocReaderOptions = defaultHakyllReaderOptions
+
+pandocWriterOptions :: WriterOptions
+pandocWriterOptions =
+    defaultHakyllWriterOptions
+        { writerHTMLMathMethod = MathJax ""
+        }
+
+pandoc :: Compiler (Item String)
+pandoc = pandocCompilerWith pandocReaderOptions pandocWriterOptions
 
 static :: Pattern -> Rules ()
 static f = match f $ do
@@ -25,23 +77,49 @@ main = hakyllWith config $ do
         route idRoute
         compile copyFileCompiler
 
+    {-
     match "css/*" $ do
         route idRoute
         compile compressCssCompiler
+        -}
 
     match (fromList ["about.rst", "disclaim.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ pandoc
             >>= loadAndApplyTemplate "templates/title.html" defaultContext
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
+
+    -- SEE: http://javran.github.io/posts/2014-03-01-add-tags-to-your-hakyll-blog.html
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $ do
+            -- SEE: https://github.com/robwhitaker/hakyll-portfolio-blog/blob/729f2d51a1ff0d4f63e6a5cf4fc1b42cd6468d0b/site.hs#L146
+            let ctx = tagsFieldNonEmpty "tags" tags <> postCtx
+
+            pandoc
+                >>= loadAndApplyTemplate "templates/post.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
+
+    tagsRules tags $ \tag tagPattern -> do
+        let title = "Posts tagged \"" ++ tag ++ "\""
+        route idRoute
+        compile $ do
+            posts <- loadAll tagPattern >>= recentFirst
+
+            let ctx =
+                    constField "title" title
+                    <> listField "posts" postCtx (return posts)
+                    <> defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/tag.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
 
     create ["blog.html"] $ do
         route idRoute
@@ -72,6 +150,22 @@ main = hakyllWith config $ do
     match "templates/*" $ compile templateCompiler
 
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" <> defaultContext
+
+tagsFieldNonEmpty :: String -> Tags -> Context a
+tagsFieldNonEmpty =
+    tagsFieldWith getTagsNonEmpty simpleRenderLink (mconcat . intersperse " ")
+    where
+        getTagsNonEmpty :: Identifier -> Compiler [String]
+        getTagsNonEmpty identifier = do
+            tags <- getTags identifier
+            if null tags then empty else return tags
+
+        simpleRenderLink :: String -> (Maybe FilePath) -> Maybe H.Html
+        simpleRenderLink _ Nothing = Nothing
+        simpleRenderLink tag (Just filePath) =
+            Just
+            $ H.a
+                ! A.class_ "badge badge-light"
+                ! A.href (toValue $ toUrl filePath)
+            $ toHtml (tag)
